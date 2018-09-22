d4f5f8240153f57a702a84eb621f9b87f4702f1c
Agustín Herranz <agustin.herranz@gmail.com>
2018-09-22 23:02:16 +0200

    Step 5.
    Change back the generator comprehension to list comprehension as we use
    the values twice.

--- string_calculator.py
+++ string_calculator.py
@@ -13,5 +13,10 @@ class StringCalculator:
             delimiter = lines[0][2:]
             lines = lines[1:]
 
-        number_list = (int(n) for line in lines for n in line.split(delimiter))
+        number_list = [int(n) for line in lines for n in line.split(delimiter)]
+
+        negative_numbers = list(filter(lambda x: x < 0, number_list))
+        if len(negative_numbers) > 0:
+            raise ValueError("negativos no soportados {}".format(",".join(map(str, negative_numbers))))
+
         return sum(number_list)
--- test_string_calculator.py
+++ test_string_calculator.py
@@ -34,3 +34,18 @@ def test_add_numbers_step4(sc, numbers, output):
     result = sc.add(numbers)
 
     assert result == output
+
+
+@pytest.mark.parametrize("numbers,negative_numbers", [
+    ("1\n2,-2", "-2"),
+    ("1,2,-3,4,-5,-6,7,8", "-3,-5,-6"),
+    ("//j\n1\n-2\n3j5\n5", "-2"),
+    ("//j\n5j1\n-2\n3\n5", "-2")
+])
+def test_add_numbers_step5(sc, numbers, negative_numbers):
+
+    with pytest.raises(ValueError) as excinfo:
+        sc.add(numbers)
+
+    assert "negativos no soportados" in str(excinfo.value)
+    assert negative_numbers in str(excinfo.value)
